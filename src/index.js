e70f8c0cd36b649fb37be26c97634ab00d5a7311
lbbboy <lbbboy@gmail.com>
2024-08-15 06:54:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.vernorboy.gq": dockerHub,
+  "quay.vernorboy.gq": "https://quay.io",
+  "gcr.vernorboy.gq": "https://gcr.io",
+  "k8s-gcr.vernorboy.gq": "https://k8s.gcr.io",
+  "k8s.vernorboy.gq": "https://registry.k8s.io",
+  "ghcr.vernorboy.gq": "https://ghcr.io",
+  "cloudsmith.vernorboy.gq": "https://docker.cloudsmith.io",
+  "ecr.vernorboy.gq": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.vernorboy.gq": dockerHub,
 };
 
 function routeByHosts(host) {
